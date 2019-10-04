0eb4e310cfd0cfa157f848c6b85ec70b9996f431
Mason_Li <misspog42@gmail.com>
2019-10-04 11:22:33 +0800

    version2.4 delete row rules and pls do not move row address of game directly

--- project1.cpp
+++ project1.cpp
@@ -6,28 +6,28 @@ int m,n,mm,nn;
 int hit_check_row(int space, int** game,int** game_blocks,int start_col,int pos){
   for(int i=4; i < m+4; ++i){
     if(game[i][start_col]==1 && game_blocks[3-space][0]==1){
-        cout<<space<<" "<<0<<endl;
-       cout<<i-1+space<<endl;
+       //cout<<space<<" "<<0<<endl;
+       //cout<<i-1+space<<endl;
       return i-1+space;
     }
     if(game[i][start_col+1]==1 && game_blocks[3-space][1]==1){
-      cout<<space<<" "<<1<<endl;
-      cout<<i-1+space<<endl;
+      //cout<<space<<" "<<1<<endl;
+      //cout<<i-1+space<<endl;
       return i-1+space;
     }
     if(game[i][start_col+2]==1 && game_blocks[3-space][2]==1){
-      cout<<space<<" "<<2<<endl;
-        cout<<i-1+space<<endl;
+      //cout<<space<<" "<<2<<endl;
+      //  cout<<i-1+space<<endl;
       return i-1+space;
     }
     if(game[i][start_col+3]==1 && game_blocks[3-space][3]==1){
-      cout<<space<<" "<<3<<endl;
-       cout<<i-1+space<<endl;
+      //cout<<space<<" "<<3<<endl;
+      // cout<<i-1+space<<endl;
       return i-1+space;
     }
   }
-  cout<<space<<" "<<start_col<<endl;
-  cout<<pos<<endl;
+  //cout<<space<<" "<<start_col<<endl;
+  //cout<<pos<<endl;
   return pos;
 }
 void blocks(char* a,int** game_blocks){
@@ -141,7 +141,6 @@ void fall_blocks(int** game,int** game_blocks){
   //fall judge and concate properly//////////////////////////
  int pos=-5;
  for(int i=0; i < 4; ++i){
-   
    pos=hit_check_row(i,game,game_blocks,start_col,pos);
    if (pos!=-5)break;
  }
@@ -149,18 +148,42 @@ void fall_blocks(int** game,int** game_blocks){
  if (pos==-5){
    pos=m+3;
  }
- cout<<"update"<<pos-3<<" "<<start_col<<endl;
+ //cout<<"update"<<pos-3<<" "<<start_col<<endl;
+ 
  for(int i =0; i <4; ++i)
    for(int j = 0; j <4; ++j){
-     
-     if(game[pos+i-3][j+start_col]!=1)game[pos+i-3][j+start_col]=game_blocks[i][j];//has problem
+     //cout<<"X";
+     if(game[pos+i-3][j+start_col]!=1)game[pos+i-3][j+start_col]=game_blocks[i][j];
     }
-
+ //cout<<endl;
 }
 void delete_satisfied_rows(int** game){
-  /*TODO
-  //delete satisfied row and update properly
-  */
+  
+  int k=4;
+  int signal=1;
+
+  while(k<m+4){
+    signal=1;
+
+    for(int i=0; i<n;i++){
+      if(game[k][i]==0){ signal=0;break;}
+    }
+    if( signal==1){
+      //cout<<"delete row"<<k<<endl;
+
+      for(int i=k; i>0;i--)
+        for(int j = 0; j <n; ++j){
+          game[i][j]=game[i-1][j];
+        }
+      for(int i=0; i<n;i++){
+       game[0][i]=0;
+      }  
+      k=4;    
+    }
+
+    k++;
+  }
+  //cout<<"delete signal"<<signal<<endl;
 }
 bool judge_game_result(int** game){
   /*TODO
@@ -177,20 +200,22 @@ int main()
   ///////////////////////////////////////////////////////////////////////
   int** game = new int*[m+4];//double pointer record pointer(for each row)
   for(int i = 0; i < m+4; ++i)game[i] = new int[n+3];//allocate elements space of each row
+
   for(int i = 0; i < m+4; ++i)
     for(int j = 0; j <n+3; ++j){
       game[i][j]=0;
     }
+
   int** game_blocks =new int*[4];
   for(int i = 0; i < 4; ++i)game_blocks[i] = new int[4];
   ///////////////////////////////////////////////////////////////////////
   bool judge=true;
   while(judge){
     fall_blocks(game,game_blocks);
-    //delete_satisfied_rows(game);
+    delete_satisfied_rows(game);
     //judge=judge_game_result(game);
     //if(!(judge)){
-      for(int i = 4; i < m+4; ++i){
+      for(int i = 0; i < m+4; ++i){
           for(int j = 0; j <n; ++j)cout<<game[i][j];
           cout<<endl;
 
